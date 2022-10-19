6ad25b220778a86950be8ea2ca131eda4aadeb15
zombieMaybeLikeCake <possibilum@gmail.com>
2022-10-20 05:47:32 +0800

    hi

--- homework2/PlayerGroupView.swift
+++ homework2/PlayerGroupView.swift
@@ -6,15 +6,35 @@
 //
 
 import SwiftUI
-
 struct PlayerGroupView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationView{
+            List(0..<6){index in
+                ForEach(1..<2){ index  in
+                    NavigationLink{
+                        PlayerView()
+                    }
+                label:{
+                    HStack{
+                        Rectangle().frame(width: 100, height: 100).aspectRatio(1,contentMode: .fit).overlay(Image("taiwan").resizable().scaledToFill())
+                        Text("台灣")
+                    }
+                    
+                    }
+                }
+             
+            }.toolbar{
+                ToolbarItem(placement: .principal){
+                    Text( "各國隊伍介紹").font(.title)
+                }
+            }
+        }.navigationTitle("各國隊伍介紹")
     }
 }
 
 struct PlayerGroupView_Previews: PreviewProvider {
     static var previews: some View {
         PlayerGroupView()
+            .previewInterfaceOrientation(.portrait)
     }
 }
